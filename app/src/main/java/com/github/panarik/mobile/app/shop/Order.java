907fbef196bc203945f6d34f71ab64aaa09e6980
panarik <panarik@yandex.ru>
2019-10-23 22:21:13 +0300

    Revert "Revert "New activityes, collection order data, send mails""
    This reverts commit 82b678da514671684536447022447d834919f63a.

---
+++ app/src/main/java/com/github/panarik/mobile/app/shop/Order.java
@@ -0,0 +1,10 @@
+package com.github.panarik.mobile.app.shop;
+
+public class Order {
+
+    String userName; //имя пользователя
+    String goodsName; //выбранный товар
+    int quantity; //выбранное количество товара
+    double orderPrice; //текущая стоимость товара
+
+}
