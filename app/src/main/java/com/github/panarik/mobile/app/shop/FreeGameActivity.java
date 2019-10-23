bc722e879253207e7625b58582116d6090de8fcf
panarik <panarik@yandex.ru>
2019-10-23 23:57:31 +0300

    2019.10.23 (23:57)

--- app/src/main/java/com/github/panarik/mobile/app/shop/FreeGameActivity.java
+++ app/src/main/java/com/github/panarik/mobile/app/shop/FreeGameActivity.java
@@ -3,6 +3,8 @@ package com.github.panarik.mobile.app.shop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FreeGameActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class FreeGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free_game);
+
+
+    }
+
+    public void chest(View view) {
+        ImageView chestClosed = findViewById(R.id.chestClosed);
+        ImageView chestEmpty = findViewById(R.id.chestEmpty);
+        ImageView chestGold = findViewById(R.id.chestGold);
+
+        int chanse = (int) (Math.random() * 4);
+        if (chanse <= 2) {
+            chestClosed.animate().alpha(0).setDuration(1);
+            chestEmpty.animate().alpha(100).setDuration(500);
+        }
+        else if (chanse > 2){
+            chestClosed.animate().alpha(0).setDuration(1);
+            chestGold.animate().alpha(100).setDuration(500);
+        }
     }
 }
