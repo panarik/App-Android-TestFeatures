b675e19d71d86d20c4b25bc28754c109e724facf
panarik <panarik@yandex.ru>
2019-10-29 00:23:36 +0300

    2019.10.29

--- app/src/main/java/com/github/panarik/mobile/app/shop/anotherThingsActivity.java
+++ app/src/main/java/com/github/panarik/mobile/app/shop/anotherThingsActivity.java
@@ -3,6 +3,7 @@ package com.github.panarik.mobile.app.shop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -21,54 +22,69 @@ public class anotherThingsActivity extends AppCompatActivity implements View.OnC
 
         ImageView fliper = findViewById(R.id.fliper);
         ImageView resizeView = findViewById(R.id.resizeView);
-        ImageView flowerImageView = findViewById(R.id.resizeView);
+        ImageView flowerImageView1 = findViewById(R.id.flowerImageView1);
+        ImageView flowerImageView2 = findViewById(R.id.flowerImageView2);
+        ImageView flowerImageView3 = findViewById(R.id.flowerImageView3);
+        ImageView flowerImageView4 = findViewById(R.id.flowerImageView4);
+        flowerImageView2.animate().alpha(0).setDuration(0);
+        flowerImageView3.animate().alpha(0).setDuration(0);
+        flowerImageView4.animate().alpha(0).setDuration(0);
+
         fliper.setOnClickListener(this);
         resizeView.setOnClickListener(this);
-        flowerImageView.setOnClickListener(this);
-
-
-    }
-/*
-
-    public void flip(View view) {
-        ImageView fliper = findViewById(R.id.fliper);
-        fliper.animate().rotation(180).setDuration(500);
+        flowerImageView1.setOnClickListener(this);
     }
 
-    public void zoom(View view) {
-        ImageView resizeView = findViewById(R.id.resizeView);
-        resizeView.animate().scaleY(0.2f).scaleX(0.2f).rotation(720).translationXBy(300).setDuration(1000);
-    }
-*/
 
     @Override
     public void onClick(View v) {
 
         ImageView fliper = findViewById(R.id.fliper);
         ImageView resizeView = findViewById(R.id.resizeView);
-        ImageView flowerImageView = findViewById(R.id.resizeView);
+        ImageView flowerImageView = findViewById(R.id.flowerImageView1);
+        ImageView flowerImageView2 = findViewById(R.id.flowerImageView2);
+        ImageView flowerImageView3 = findViewById(R.id.flowerImageView3);
+        ImageView flowerImageView4 = findViewById(R.id.flowerImageView4);
 
         switch (v.getId()) // Метод view.getId()
         {
             case R.id.fliper:
                 statFliper = statFliper + 1;
-
                 if (statFliper % 2 == 0) {
                     fliper.animate().rotation(180).setDuration(1000);
                 } else {
                     fliper.animate().rotation(0).setDuration(1000);
                 }
                 break;
+
             case R.id.resizeView:
                 statResizeView = statResizeView + 1;
-
                 if (statResizeView % 2 == 0) {
-                    flowerImageView.animate().scaleY(1f).scaleX(1f).setDuration(1000);
+                    resizeView.animate().scaleY(2f).scaleX(2f).setDuration(500);
                 } else {
-                    resizeView.animate().scaleY(3f).scaleX(3f).setDuration(1000);
+                    resizeView.animate().scaleY(1f).scaleX(1f).setDuration(500);
                 }
                 break;
 
+            case R.id.flowerImageView1:
+
+                if (statFlowerImageView  == 1) {
+                    flowerImageView.animate().alpha(0).setDuration(500);
+                    flowerImageView2.animate().alpha(100).setDuration(8000);
+                    statFlowerImageView = statFlowerImageView + 1;
+                    Log.d("statFlowerImageView", "" + statFlowerImageView);
+                } else if (statFlowerImageView  == 2) {
+                    flowerImageView2.animate().alpha(0).setDuration(500);
+                    flowerImageView3.animate().alpha(100).setDuration(8000);
+                    statFlowerImageView = statFlowerImageView + 1;
+                    Log.d("statFlowerImageView", "" + statFlowerImageView);
+                } else if(statFlowerImageView  == 3){
+                    flowerImageView3.animate().alpha(0).setDuration(0);
+                    flowerImageView4.animate().alpha(100).setDuration(8000);
+                    statFlowerImageView = statFlowerImageView + 1;
+                    Log.d("statFlowerImageView", "" + statFlowerImageView);
+                }
+                break;
 
         }
     }
