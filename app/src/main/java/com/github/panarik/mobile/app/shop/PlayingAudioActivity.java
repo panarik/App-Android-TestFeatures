a66861eec0a50cb2fc72132a02f8ff821839b6f7
panarik <panarik@yandex.ru>
2019-10-30 23:38:13 +0300

    2019.10.30 (23:37)

--- app/src/main/java/com/github/panarik/mobile/app/shop/PlayingAudioActivity.java
+++ app/src/main/java/com/github/panarik/mobile/app/shop/PlayingAudioActivity.java
@@ -2,26 +2,66 @@ package com.github.panarik.mobile.app.shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class PlayingAudioActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
+    String control = "Play";
+    SeekBar volumeSeekBar;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playing_audio);
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.audio);
+        volumeSeekBar = findViewById(R.id.volumeSeekBar);
+
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        volumeSeekBar.setMax(maxVolume);
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("Progress_changed", "" + progress);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
-    public void Play(View view) {
-        mediaPlayer.start();
-    }
+    public void Control(View view) {
 
-    public void Pause(View view) {
-        mediaPlayer.pause();
+        Button buttonControl = findViewById(R.id.buttonControl);
+
+        switch (control) {
+            case "Play":
+                mediaPlayer.start();
+                buttonControl.setText("Pause");
+                control = "Pause";
+                break;
+            case "Pause":
+                mediaPlayer.pause();
+                buttonControl.setText("Play");
+                control = "Play";
+                break;
+        }
     }
 }
