9b40be90ffbc8be32e62463aa48ff3cd79144434
panarik <panarik@yandex.ru>
2019-11-22 23:06:32 +0300

    2019.11.22

--- app/src/main/java/com/github/panarik/mobile/app/shop/FreeGameActivity.java
+++ app/src/main/java/com/github/panarik/mobile/app/shop/FreeGameActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.github.panarik.mobile.app.shop.menu.MenuActivity;
+
 public class FreeGameActivity extends AppCompatActivity {
 
 
@@ -49,5 +51,11 @@ public class FreeGameActivity extends AppCompatActivity {
     public void anotherThings(View view) {
         Intent anotherThings = new Intent(FreeGameActivity.this, anotherThingsActivity.class);
         startActivity(anotherThings);
+    }
+
+    public void goToMenuActivity(View view) {
+
+        Intent goToMenuActivity = new Intent (FreeGameActivity.this, MenuActivity.class);
+        startActivity(goToMenuActivity);
     }
 }
---
+++ app/src/main/java/com/github/panarik/mobile/app/shop/menu/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.github.panarik.mobile.app.shop.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.panarik.mobile.app.shop.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
