161f2ba1583f197f534d61a45f5ffe9b97bc5eda
panarik <panarik@yandex.ru>
2019-11-23 20:46:49 +0300

    2019.11.23 (20:00)

--- app/src/main/java/com/github/panarik/mobile/app/shop/menu/MenuActivity.java
+++ app/src/main/java/com/github/panarik/mobile/app/shop/menu/MenuActivity.java
@@ -2,15 +2,23 @@ package com.github.panarik.mobile.app.shop.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.github.panarik.mobile.app.shop.R;
+import com.github.panarik.mobile.app.shop.menu.createinterface.CreateInterfaceActivity;
+
+import java.util.ArrayList;
 
 public class MenuActivity extends AppCompatActivity {
 
-    String[] themes = {"№1 - Создание интерфейса", "№2 - Основные элементы управления", "№3 - Ресурсы"};
+    ListView listMenuView;
+    ArrayList<String> arrayMenuList;
+    ArrayAdapter arrayMenuAdapter;
 
 
     @Override
@@ -18,8 +26,27 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        ListView themesList = (ListView) findViewById(R.id.themesListView);
-        ArrayAdapter themesAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, themes);
-        themesList.setAdapter(themesAdapter);
+        listMenuView = findViewById(R.id.themesListView);
+        arrayMenuList = new ArrayList<>();
+        arrayMenuList.add("Создание интерфейса"); // №0
+        arrayMenuList.add("Элементы управления"); // №1
+        arrayMenuList.add("Ресурсы"); // №2
+
+        arrayMenuAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayMenuList);
+        listMenuView.setAdapter(arrayMenuAdapter);
+
+        listMenuView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position) {
+                    case 0:
+                        Intent goToCreateInterface = new Intent(MenuActivity.this, CreateInterfaceActivity.class);
+                        startActivity(goToCreateInterface);
+                        break;
+                }
+            }
+        });
     }
 }
+
---
+++ app/src/main/java/com/github/panarik/mobile/app/shop/menu/createinterface/CreateInterfaceActivity.java
@@ -0,0 +1,16 @@
+package com.github.panarik.mobile.app.shop.menu.createinterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.panarik.mobile.app.shop.R;
+
+public class CreateInterfaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_interface);
+    }
+}
