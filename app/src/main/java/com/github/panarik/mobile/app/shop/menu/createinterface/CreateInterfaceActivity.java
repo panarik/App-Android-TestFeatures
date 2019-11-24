1219a8964f8f24940b9bd82b8a9066672a5996af
panarik <panarik@yandex.ru>
2019-11-24 23:48:59 +0300

    2019.11.24 23:00

--- app/src/main/java/com/github/panarik/mobile/app/shop/menu/createinterface/CreateInterfaceActivity.java
+++ app/src/main/java/com/github/panarik/mobile/app/shop/menu/createinterface/CreateInterfaceActivity.java
@@ -2,7 +2,11 @@ package com.github.panarik.mobile.app.shop.menu.createinterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.print.PrintAttributes;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.github.panarik.mobile.app.shop.R;
@@ -14,14 +18,50 @@ public class CreateInterfaceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_interface);
 /*
-
         TextView textView = new TextView(this);
         textView.setText("Пример №1 - Вывод текста силами Java без верстки в XML");
         textView.setTextSize(16);
         setContentView(textView);
 */
 
+/*
         TextView textInterfaceView1 = findViewById(R.id.textInterfaceView1);
         textInterfaceView1.setText("Пример №2 - Вывод текста силами Java c версткой в XML");
+*/
+
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        TextView interfaceTextView3 = new TextView(this);
+        TextView interfaceTextView4 = new TextView(this);
+
+        interfaceTextView3.setText("Пример №3 - Вывод текста в контейнере RelativeLayout силами Java класса");
+        interfaceTextView3.setBackgroundColor(Color.GRAY);
+        interfaceTextView3.setTextColor(Color.BLACK);
+        interfaceTextView3.setTextSize(16);
+
+        interfaceTextView4.setText("Пример №4 - Вывод текста в контейнере RelativeLayout силами Java класса");
+        interfaceTextView4.setBackgroundColor(Color.GREEN);
+        interfaceTextView4.setTextColor(Color.BLACK);
+        interfaceTextView4.setTextSize(16);
+
+        //создаём объект с параметами
+        RelativeLayout.LayoutParams layoutParams3 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 200);
+        layoutParams3.setMargins(30,30,30,30);
+
+        RelativeLayout.LayoutParams layoutParams4 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 200);
+        layoutParams4.setMargins(30,250,30,30);
+
+        relativeLayout.setBackgroundColor(Color.LTGRAY);
+
+        //применяем параметры ко всем textInterfaceView
+        interfaceTextView3.setLayoutParams(layoutParams3);
+        interfaceTextView3.setPadding(30,30,30,30);
+        interfaceTextView4.setLayoutParams(layoutParams4);
+        interfaceTextView3.setPadding(30,30,30,30);
+
+        relativeLayout.addView(interfaceTextView3);
+        relativeLayout.addView(interfaceTextView4);
+
+        setContentView(relativeLayout);
+
     }
 }
