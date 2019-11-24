a85804dbe9ef06c78c7faaba270d2957c26add87
panarik <panarik@yandex.ru>
2019-11-24 17:21:43 +0300

    2019.11.24

--- app/src/main/java/com/github/panarik/mobile/app/shop/menu/createinterface/CreateInterfaceActivity.java
+++ app/src/main/java/com/github/panarik/mobile/app/shop/menu/createinterface/CreateInterfaceActivity.java
@@ -3,6 +3,7 @@ package com.github.panarik.mobile.app.shop.menu.createinterface;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.github.panarik.mobile.app.shop.R;
 
@@ -12,5 +13,15 @@ public class CreateInterfaceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_interface);
+/*
+
+        TextView textView = new TextView(this);
+        textView.setText("Пример №1 - Вывод текста силами Java без верстки в XML");
+        textView.setTextSize(16);
+        setContentView(textView);
+*/
+
+        TextView textInterfaceView1 = findViewById(R.id.textInterfaceView1);
+        textInterfaceView1.setText("Пример №2 - Вывод текста силами Java c версткой в XML");
     }
 }
