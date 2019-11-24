a91560a70306eb3ed5d7c12fb1f4ce88e89df05f
panarik <panarik@yandex.ru>
2019-11-25 00:30:44 +0300

    2019.11.25

--- app/src/main/java/com/github/panarik/mobile/app/shop/menu/createinterface/CreateInterfaceActivity.java
+++ app/src/main/java/com/github/panarik/mobile/app/shop/menu/createinterface/CreateInterfaceActivity.java
@@ -2,10 +2,13 @@ package com.github.panarik.mobile.app.shop.menu.createinterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.print.PrintAttributes;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -32,6 +35,7 @@ public class CreateInterfaceActivity extends AppCompatActivity {
         RelativeLayout relativeLayout = new RelativeLayout(this);
         TextView interfaceTextView3 = new TextView(this);
         TextView interfaceTextView4 = new TextView(this);
+        Button interfaceButton1 = new Button(this);
 
         interfaceTextView3.setText("Пример №3 - Вывод текста в контейнере RelativeLayout силами Java класса");
         interfaceTextView3.setBackgroundColor(Color.GRAY);
@@ -43,25 +47,41 @@ public class CreateInterfaceActivity extends AppCompatActivity {
         interfaceTextView4.setTextColor(Color.BLACK);
         interfaceTextView4.setTextSize(16);
 
-        //создаём объект с параметами
+        //создаём объекты с параметами
         RelativeLayout.LayoutParams layoutParams3 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 200);
-        layoutParams3.setMargins(30,30,30,30);
+        layoutParams3.setMargins(30, 30, 30, 30);
 
         RelativeLayout.LayoutParams layoutParams4 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 200);
-        layoutParams4.setMargins(30,250,30,30);
+        layoutParams4.setMargins(30, 250, 30, 30);
 
-        relativeLayout.setBackgroundColor(Color.LTGRAY);
+        RelativeLayout.LayoutParams layoutParamsButton1 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 150);
+        layoutParamsButton1.setMargins(30, 1500, 30, 30);
 
-        //применяем параметры ко всем textInterfaceView
+
+        //применяем параметры ко всем View
         interfaceTextView3.setLayoutParams(layoutParams3);
-        interfaceTextView3.setPadding(30,30,30,30);
+        interfaceTextView3.setPadding(30, 30, 30, 30);
         interfaceTextView4.setLayoutParams(layoutParams4);
-        interfaceTextView3.setPadding(30,30,30,30);
+        interfaceTextView3.setPadding(30, 30, 30, 30);
+        interfaceButton1.setLayoutParams(layoutParamsButton1);
+        interfaceButton1.setBackgroundColor(Color.GRAY);
+        interfaceButton1.setText("go To Another ViewGroups");
+
+        interfaceButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoLinearLayoutActivity = new Intent(CreateInterfaceActivity.this, LinearLayoutActivity.class);
+                startActivity(gotoLinearLayoutActivity);
+            }
+        });
 
         relativeLayout.addView(interfaceTextView3);
         relativeLayout.addView(interfaceTextView4);
+        relativeLayout.addView(interfaceButton1);
+        relativeLayout.setBackgroundColor(Color.LTGRAY);
 
         setContentView(relativeLayout);
-
     }
+
+
 }
