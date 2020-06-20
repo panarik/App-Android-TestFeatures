6aeba4338182efe43160af95edf6dff70f14b288
panarik <panarik@yandex.ru>
2020-06-20 23:31:48 +0300

    [WebVideo] create movie adapter

--- app/src/main/java/com/github/panarik/mobile/app/shop/data/videoweb/Test.java
+++
@@ -1,4 +0,0 @@
-package com.github.panarik.mobile.app.shop.data.videoweb;
-
-public class Test {
-}
--- app/src/main/java/com/github/panarik/mobile/app/shop/data/videoweb/model/Movie.java
+++ app/src/main/java/com/github/panarik/mobile/app/shop/data/videoweb/model/Movie.java
@@ -2,6 +2,35 @@ package com.github.panarik.mobile.app.shop.data.videoweb.model;
 
 public class Movie {
 
+private String posterUrl;
+private String title;
+private String year;
+
+    public Movie() {
+    }
 
 
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
 }
