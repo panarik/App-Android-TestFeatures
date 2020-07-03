69c774e291298f63770c7e395c2140699788d07d
panarik <panarik@yandex.ru>
2020-07-03 21:42:09 +0300

    [Feature]Crashlytics - add SDK

--- app/src/main/java/com/github/panarik/mobile/app/shop/activity/ChatActivity.java
+++ app/src/main/java/com/github/panarik/mobile/app/shop/activity/ChatActivity.java
@@ -3,6 +3,9 @@ package com.github.panarik.mobile.app.shop.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.github.panarik.mobile.app.shop.R;
 
@@ -12,5 +15,18 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        Button crashButton = new Button(this);
+        crashButton.setText("Crash!");
+        crashButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                throw new RuntimeException("Test Crash"); // Force a crash
+            }
+        });
+
+        addContentView(crashButton, new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+
     }
 }
