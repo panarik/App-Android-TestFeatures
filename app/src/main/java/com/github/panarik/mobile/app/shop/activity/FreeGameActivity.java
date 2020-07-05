8c6c01496e76bab1510a827909fed8bb4e0036c9
panarik <panarik@yandex.ru>
2020-07-05 20:47:30 +0300

    [Fix]FreeGameActivity - refactoring

--- app/src/main/java/com/github/panarik/mobile/app/shop/activity/FreeGameActivity.java
+++ app/src/main/java/com/github/panarik/mobile/app/shop/activity/FreeGameActivity.java
@@ -12,6 +12,11 @@ import com.github.panarik.mobile.app.shop.R;
 
 public class FreeGameActivity extends AppCompatActivity {
 
+    private ImageView chestClosed;
+    private ImageView chestEmpty;
+    private ImageView chestGold;
+    private Button tryAgain;
+    private ImageView youVinView;
 
 
     @Override
@@ -19,29 +24,32 @@ public class FreeGameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free_game);
 
+        chestClosed = findViewById(R.id.chestClosed);
+        chestEmpty = findViewById(R.id.chestEmpty);
+        chestGold = findViewById(R.id.chestGold);
+        tryAgain = findViewById(R.id.tryAgain);
+        youVinView = findViewById(R.id.youVinView);
+
+        tryAgain.setVisibility(Button.INVISIBLE);
+
 
     }
 
     public void chest(View view) {
-        ImageView chestClosed = findViewById(R.id.chestClosed);
-        ImageView chestEmpty = findViewById(R.id.chestEmpty);
-        ImageView chestGold = findViewById(R.id.chestGold);
-        Button tryAgain = findViewById(R.id.tryAgain);
-        ImageView youVinView = findViewById(R.id.youVinView);
+
 
         int chanse = (int) (Math.random() * 4);
         if (chanse <= 2) {
-            // someView.setVisibility(View.INVISIBLE);
-            // someView.setVisibility(View.VISIBLE);
             chestClosed.animate().alpha(0).setDuration(1);
             chestEmpty.animate().alpha(100).setDuration(500);
-            tryAgain.animate().alpha(100).setDuration(500);
+            tryAgain.setVisibility(Button.VISIBLE);
 
         }
         else if (chanse > 2){
             chestClosed.animate().alpha(0).setDuration(1);
             chestGold.animate().alpha(100).setDuration(500);
             youVinView.animate().alpha(100).setDuration(500);
+            tryAgain.setVisibility(Button.INVISIBLE);
         }
     }
 
