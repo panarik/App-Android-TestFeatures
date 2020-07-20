f86693a9d1c9ebe6300a2a622440bd1d7118997d
panarik <panarik@yandex.ru>
2020-07-20 21:52:27 +0300

    [Test]TestSignInBase - add test_auth_signUp

--- app/src/androidTest/java/com/github/panarik/smartFeatures/screen/TestSignInBase.java
+++ app/src/androidTest/java/com/github/panarik/smartFeatures/screen/TestSignInBase.java
@@ -5,6 +5,8 @@ import com.github.panarik.smartFeatures.base.TestBase;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
@@ -28,6 +30,24 @@ public class TestSignInBase extends TestBase {
     public void test_auth_signIn(){
         auth_signIn();
         //находимся в MainActivity
+        onView(withId(R.id.activity_main)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_auth_signUp(){
+        //вводим новый email
+        final Random random = new Random();
+        onView(withId(R.id.chat_emailEditText)).perform(typeText
+                ("test"+random.nextInt(900)+"@gmail.com"));
+        //вводим пароль
+        onView(withId(R.id.chat_passwordEditText)).perform(typeText("123456"));
+        //вводим подтверждение пароля
+        onView(withId(R.id.chat_passwordConfirmEditText)).perform(typeText("123456"));
+        //жмем кнопку Далее
+        onView(withId(R.id.chat_loginSignUpButton)).perform(click());
+        //ждем
+        onView(isRoot()).perform(waitFor(3000));
+        //переход на MainActivity
         onView(withId(R.id.activity_main)).check(matches(isDisplayed()));
     }
 
