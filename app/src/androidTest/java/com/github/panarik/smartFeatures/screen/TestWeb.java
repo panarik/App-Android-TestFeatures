0852a13cf946dffac38ea2e019286c7eb1fda0f9
panarik <panarik@yandex.ru>
2020-08-06 01:03:02 +0300

    [Test]TestWeb add input text test

--- app/src/androidTest/java/com/github/panarik/smartFeatures/screen/TestWeb.java
+++ app/src/androidTest/java/com/github/panarik/smartFeatures/screen/TestWeb.java
@@ -10,7 +10,6 @@ import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
 import com.github.panarik.smartFeatures.R;
-import com.github.panarik.smartFeatures.activity.SignInActivity;
 import com.github.panarik.smartFeatures.activity.WebActivity;
 
 import org.hamcrest.Matcher;
@@ -18,16 +17,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.web.assertion.WebViewAssertions.webMatches;
 import static androidx.test.espresso.web.sugar.Web.onWebView;
 import static androidx.test.espresso.web.webdriver.DriverAtoms.findElement;
 import static androidx.test.espresso.web.webdriver.DriverAtoms.getText;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.webKeys;
 import static org.hamcrest.Matchers.containsString;
 
 @LargeTest
@@ -42,31 +38,31 @@ public class TestWeb {
         }
     };
 
-    private static final String text = "Россия";
-
 
     @Test
-    public void test_WebView_classQ8LRLc_ID_withText() {
+    public void test_WebView_ID_withText() {
         onWebView()
                 .withElement(findElement(Locator.ID,"fsl"))
                 .check(webMatches(getText(), containsString("Россия")));
     }
 
     @Test
-    public void test_WebView_classQ8LRLc_XPATH_withText(){
-
+    public void test_WebView_XPATH_withText(){
         onWebView()
                 .withElement(findElement(Locator.XPATH,"//span[@class='Q8LRLc']"))
                 .check(webMatches(getText(), containsString("Россия")));
     }
 
 
-    private void goToWeb() {
-        auth_signIn();
-        onView(withId(R.id.recyclerView))
-                .perform(actionOnItemAtPosition(9, click()));
-        onView(isRoot()).perform(waitFor(1000));
+    @Test
+
+    public void test_WebView_search_withText(){
+        onWebView()
+                .withElement(findElement(Locator.XPATH, "//input[@class='gLFyf']"))
+                .perform(webKeys("abcd"));
     }
+
+
 
 
     //флоу signIn
@@ -89,19 +85,5 @@ public class TestWeb {
         };
     }
 
-
-    //флоу signIn
-    public static void auth_signIn() {
-        //переход в режим signIn
-        onView(withId(R.id.chat_toggleSingUpTextView)).perform(click());
-        //вводим email
-        onView(withId(R.id.chat_emailEditText)).perform(typeText("test2@gmail.com"));
-        //вводим пароль
-        onView(withId(R.id.chat_passwordEditText)).perform(typeText("123456"));
-        //жмем далее
-        onView(withId(R.id.chat_loginSignUpButton)).perform(click());
-        //ждем
-        onView(isRoot()).perform(waitFor(3000));
-    }
 
 }
