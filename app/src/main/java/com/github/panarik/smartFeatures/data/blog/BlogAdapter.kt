765828369553393d0d622149aa8f0f5bf6860341
panarik <panarik@yandex.ru>
2020-09-25 18:31:05 +0300

    [Feature]BlogKotlinActivity add arraylist for titles

--- app/src/main/java/com/github/panarik/smartFeatures/data/blog/BlogAdapter.kt
+++ app/src/main/java/com/github/panarik/smartFeatures/data/blog/BlogAdapter.kt
@@ -5,30 +5,47 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.github.panarik.smartFeatures.R
+import kotlinx.android.synthetic.main.blog_item.view.*
 
-class BlogAdapter: RecyclerView.Adapter<BlogViewHolder>() //типовой RecyclerView.Adapter будет использовать кастомный BlogViewHolder
+class BlogAdapter:
+        RecyclerView.Adapter<BlogViewHolder>() //типовой RecyclerView.Adapter будет использовать кастомный BlogViewHolder
 {
+
+    //Создаём ArrayList, содержащий заголовки в вьюХолдерах
+    val blogTitles = listOf<String>("First title", "Second title", "Third title", "Another title")
+
+
 
     //создаёт отдельный ВьюХолдер
     override fun onCreateViewHolder
             (parent: ViewGroup, viewType: Int): BlogViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val blogItem = layoutInflater.inflate(R.layout.blog_item, parent, false)
+        val blogItem = layoutInflater.inflate(R.layout.blog_item, parent, false) //разметка вьюХолдера = разметка blog_item
         return BlogViewHolder(blogItem)
     }
 
+    //присваиваем ВьюХолдерам конкретные значения в полях разметки
     override fun onBindViewHolder(holder: BlogViewHolder, position: Int) {
+
+        //1ый пример. Можно присвоить текст для отдельного view кастомного ВьюХолдера
+        //holder.view.blog_title.text = "123" //
+
+        //2ой пример. Привязываем текст к ArrayList
+        val blogTitle = blogTitles.get(position)
+        holder.view.blog_title.text = blogTitle
+
+
 
     }
 
     //количество ИьюХолдеров в RecyclerView
     override fun getItemCount(): Int {
-        return 3
+        return blogTitles.size // количество строк в ArrayList blogTitles
     }
 }
 
 //Создаем свой ViewHolder
-class BlogViewHolder(v: View): RecyclerView.ViewHolder(v) {
+class BlogViewHolder(val view: View): RecyclerView.ViewHolder(view) {
 
 
 
